3c8c664c04d5982fe0e53fdf0e7b281f9d2a4ea9
Elias <eliaskiso4@gmail.com>
2023-05-18 21:42:06 -0300

    Atulizando os formulários

--- pages/cursos/index.js
+++ pages/cursos/index.js
@@ -1,44 +1,58 @@
 import Pagina from '@/components/Pagina'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap'
 import { AiOutlinePlus } from 'react-icons/ai'
+import { BsTrash3 } from 'react-icons/bs'
+
 
 const index = () => {
+
+    const [cursos, setCursos] = useState([])
+
+    useEffect(() => {
+        setCursos(getAll())
+    }, [])
+
+    function getAll(){
+        return JSON.parse(window.localStorage.getItem('cursos')) || []
+    }
+
+    function excluir(id){
+        const itens = getAll()
+        itens.splice(id, 1)
+        window.localStorage.setItem('cursos', JSON.stringify(itens))
+        setCursos(itens)
+    }
+
     return (
         <Pagina titulo='Cursos'>
 
-        <Link href="/cursos/form" className='mb-2 btn btn-primary'>
-            <AiOutlinePlus/>
-            Novo
-        </Link>
-            <Table striped bordered hover>
+            <Link href="/cursos/form" className='mb-2 btn btn-primary'>
+                <AiOutlinePlus />
+                Novo
+            </Link>
+            <Table striped bordered hover >
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+
+                        <th>Excluir</th>
+                        <th>Nome</th>
+                        <th>Duração</th>
+                        <th>Modalidade</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    {cursos.map((item, i) => (
+                        <tr key={i}>
+                            <td>
+                                <BsTrash3 onClick={() => excluir(i)} className='text-warning' />
+                            </td>
+                            <td>{item.nome}</td>
+                            <td>{item.duracao}</td>
+                            <td>{item.modalidade}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </Table>
 
