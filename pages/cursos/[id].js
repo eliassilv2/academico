339d387c4771755f9cb5cd70cbddcda21dd45140
Elias <eliaskiso4@gmail.com>
2023-05-22 22:03:08 -0300

    adicionando funções

--- pages/cursos/[id].js
+++ pages/cursos/[id].js
@@ -1,7 +1,7 @@
 import Pagina from '@/components/Pagina'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { AiOutlineCheck } from 'react-icons/ai'
@@ -9,9 +9,21 @@ import { TbArrowBack } from 'react-icons/tb'
 
 const form = () => {
 
+    const { push, query } = useRouter()
+    const [curso, setCurso] = useState({})
+    const { register, handleSubmit, setValue } = useForm()
 
-    const { push } = useRouter()
-    const { register, handleSubmit } = useForm()
+    useEffect(() => {
+
+        if (query.id) {
+            const cursos = JSON.parse(window.localStorage.getItem('cursos'))
+            const curso = cursos[query.id]
+
+            for(let atributo in curso){
+                setValue(atributo, curso[atributo])
+            }
+        }
+    }, [query.id])
 
     function salvar(dados) {
         const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []
@@ -40,11 +52,11 @@ const form = () => {
                 </Form.Group>
                 <div className='text-center'>
                     <Button variant="success" onClick={handleSubmit(salvar)}>
-                        <AiOutlineCheck className='me-2'/>
+                        <AiOutlineCheck className='me-2' />
                         Salvar
                     </Button>
                     <Link className='ms-2 btn btn-danger' href={'/cursos'}>
-                        <TbArrowBack className='me-2'/>
+                        <TbArrowBack className='me-2' />
                         Voltar
                     </Link>
                 </div>
--- pages/cursos/index.js
+++ pages/cursos/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { BsTrash3 } from 'react-icons/bs'
-
+import { HiOutlinePencilAlt } from 'react-icons/hi'
 
 const index = () => {
 
@@ -14,22 +14,24 @@ const index = () => {
         setCursos(getAll())
     }, [])
 
-    function getAll(){
+    function getAll() {
         return JSON.parse(window.localStorage.getItem('cursos')) || []
     }
 
-    function excluir(id){
-        const itens = getAll()
-        itens.splice(id, 1)
-        window.localStorage.setItem('cursos', JSON.stringify(itens))
-        setCursos(itens)
+    function excluir(id) {
+        if (confirm('Deseja realmente excluir o registro? ')) {
+            const itens = getAll()
+            itens.splice(id, 1)
+            window.localStorage.setItem('cursos', JSON.stringify(itens))
+            setCursos(itens)
+        }
     }
 
     return (
         <Pagina titulo='Cursos'>
 
             <Link href="/cursos/form" className='mb-2 btn btn-primary'>
-                <AiOutlinePlus />
+                <AiOutlinePlus className='me-1' />
                 Novo
             </Link>
             <Table striped bordered hover >
@@ -46,7 +48,11 @@ const index = () => {
                     {cursos.map((item, i) => (
                         <tr key={i}>
                             <td>
-                                <BsTrash3 onClick={() => excluir(i)} className='text-warning' />
+                                <Link href={'/cursos/' + 1}>
+                                    <HiOutlinePencilAlt title='Alterar' className='text-primary' />
+                                </Link>
+                                {' '}
+                                <BsTrash3 title='Excluir' onClick={() => excluir(i)} className='text-warning' />
                             </td>
                             <td>{item.nome}</td>
                             <td>{item.duracao}</td>
