cd0f96c028b61cc20e9e95bd64117779cef6b4d8
Elias <eliaskiso4@gmail.com>
2023-06-11 23:16:51 -0300

    adicionando mascaras nas páginas

--- validators/alunoValidator.js
+++ validators/alunoValidator.js
@@ -18,21 +18,21 @@ const alunoValidator = {
         maxLength: {
             value: 13,
             message: 'o maximo é 13'
-        },
+        }, 
     },
 
     matricula: {
         required: 'Campo Obrigatório!',
         minLength: {
-            value: 13,
-            message: 'O Minimo é 13'
+            value: 11,
+            message: 'O Minimo é 11'
         },
 
         required: 'Campo Obrigatório!',
         maxLength: {
-            value: 13,
-            message: 'o maximo é 13'
-        },  
+            value: 11,
+            message: 'o maximo é 11'
+        },   
     },
 
     email: {
